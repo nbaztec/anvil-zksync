b6d423cfcf41b5e8dd24ebf004fdf1a159b62c3e
Nisheeth Barthwal <nbaztec@gmail.com>
2025-09-09 14:30:58 +0200

    handle IExecutorV28 for older protocol versions

--- scripts/refresh_l1_sidecar_contracts.sh
+++ scripts/refresh_l1_sidecar_contracts.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -xe
 
+PROTOCOL_VERSION=${1:-v29}
+
 # Copy Solidity source files `<contract_name>.sol` from SRC to DST
 L1_SOL_SRC_DIR=contracts/l1-contracts/contracts
 L1_SOL_DST_DIR=crates/l1_sidecar/src/contracts/sol
@@ -12,6 +14,19 @@ l1_sol_contracts=("state-transition/chain-interfaces/IExecutor")
 for path in "${l1_sol_contracts[@]}"; do
   directory=$(dirname "$path")
   contract=$(basename "$path")
+
+  if [[ "$contract" == "IExecutor" ]]; then
+    version_num=${PROTOCOL_VERSION#v}
+    # If version is older than v29, then copy the IExecutor as IExecutorV28 and also change the interface name within the file.
+    # After v29 IExecutor interface is incompatible with older IExecutor interface, so it must be copied as such.
+    # See https://github.com/matter-labs/anvil-zksync/pull/734
+    if [[ "$version_num" =~ ^[0-9]+$ ]] && [[ "$version_num" -lt 29 ]]; then
+      cp "$L1_SOL_SRC_DIR/$directory/$contract.sol" "$L1_SOL_DST_DIR/IExecutorV28.sol"
+      sed -i 's/interface IExecutor/interface IExecutorV28/g' "$L1_SOL_DST_DIR/IExecutorV28.sol"
+      continue
+    fi
+  fi
+
   cp "$L1_SOL_SRC_DIR/$directory/$contract.sol" $L1_SOL_DST_DIR
 done
 
